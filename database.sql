17762d35fc036721b8f7dd55e3e738b81afae743
Jason Henrickson <jason.henrickson15@gmail.com>
2022-02-21 16:36:04 -0600

    altered database order

--- database.sql
+++ database.sql
@@ -3,27 +3,10 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
-CREATE Table "users" (
+CREATE Table "user" (
 	"id" SERIAL PRIMARY KEY,
 	"username" varchar(80) UNIQUE NOT NULL,
 	"password" varchar(1000) NOT NULL
-);
-
-CREATE TABLE "notes" (
-	"id" SERIAL PRIMARY KEY,
-	"user_id" INTEGER REFERENCES "users",
-	"car_id" INTEGER REFERENCES "cars"
-	"type" VARCHAR(40) NOT NULL,
-	"text" varchar(480)
-	"problem" VARCHAR(480),
-	"solution" VARCHAR(480),
-	"solved" BOOLEAN DEFAULT false,
-);
-
-CREATE TABLE "images" (
-	"id" SERIAL PRIMARY KEY,
-	"path" varchar(240) NOT NULL,
-	"note_id" INTEGER REFERENCES "notes"
 );
 
 CREATE TABLE "cars" (
@@ -31,4 +14,21 @@ CREATE TABLE "cars" (
 	"year" INTEGER NOT NULL,
 	"make" VARCHAR(40) NOT NULL,
 	"model" VARCHAR(40) NOT NULL
+);
+
+CREATE TABLE "notes" (
+	"id" SERIAL PRIMARY KEY,
+	"user_id" INTEGER REFERENCES "user",
+	"car_id" INTEGER REFERENCES "cars",
+	"type" VARCHAR(40) NOT NULL,
+	"text" varchar(480),
+	"problem" VARCHAR(480),
+	"solution" VARCHAR(480),
+	"solved" BOOLEAN DEFAULT false
+);
+
+CREATE TABLE "images" (
+	"id" SERIAL PRIMARY KEY,
+	"path" varchar(240) NOT NULL,
+	"note_id" INTEGER REFERENCES "notes"
 );
